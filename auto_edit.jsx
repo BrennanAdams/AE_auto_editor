1586c9a0b8856ac81a94a2238a3d3dd4ede9f6d8
Brennan Adams <brennanadams@Brennans-MacBook-Pro-2.local>
2023-05-21 22:46:50 -0600

    refactoring

--- auto_edit.jsx
+++ auto_edit.jsx
@@ -8,10 +8,54 @@ if (activeComp && activeComp instanceof CompItem) {
   // Get the selected layers in the timeline
   var selectedLayers = activeComp.selectedLayers;
 
-  //find the audio layer -----------------------------------------
-  var targetLayerName = "audio"; // Replace "LayerName" with the name of the layer you want to select
-  // Get the layers in the composition
+  var targetLayerName = "audio"
   var layers = activeComp.layers;
+
+  var audio_markers = find_audio_layer(layers,targetLayerName) //returns the audio markers in array format
+
+  // ______________________ CUTTING ALL CLIPS ______________________
+  // Check if any layers are selected
+  if (selectedLayers.length > 0) {
+
+    for (var layer_idx = 0; layer_idx <= selectedLayers.length - 1; layer_idx ++) {
+      // Get the first selected layer
+      var selectedLayer = selectedLayers[layer_idx];
+      var markerProperty = audio_markers;
+
+      if (markerProperty && markerProperty.numKeys > 0) {
+      var numMarkers = markerProperty.numKeys; // Get the number of markers
+
+      for (var i = numMarkers; i >= 2; i--) {
+        cut_layer(markerProperty, selectedLayer);
+    }
+  } else {
+    alert("No markers found on the layer.");
+  }
+    }
+  } else {
+    alert("No layer selected in the timeline.");
+  }
+
+
+  // ______________________ SELECTING AND SEQUENCING PART ______________________
+
+
+
+
+
+
+  
+
+} else {
+  alert("No composition open in Adobe After Effects.");
+}
+
+
+
+function find_audio_layer(layers, target_layer_name) {
+  // Find the audio layer
+  var targetLayerName = target_layer_name;
+  var audio_markers = null; 
   // Iterate through the layers to find the target layer
   for (var i = 1; i <= layers.length; i++) {
     var layer = layers[i];
@@ -20,53 +64,24 @@ if (activeComp && activeComp instanceof CompItem) {
     if (layer.name === targetLayerName) {
       // Select the target layer
       layer.selected = true;
-      var audio_markers = layer.property("ADBE Marker");
+      audio_markers = layer.property("ADBE Marker"); // Remove 'var' keyword to update the global variable
       break; // Exit the loop since the target layer has been found
     }
   }
-  // /---------------------------------------------------------------------------------------------------------------------------
+  return audio_markers;
+}
 
 
 
-  // Check if any layers are selected
-  if (selectedLayers.length > 0) {
+function cut_layer(markerProperty, selectedLayer) {
+  var markerTime = markerProperty.keyTime(i); // Get the marker time
+  var nextMarkerTime = markerProperty.keyTime(i - 1);
+  var duplicatedLayer = selectedLayer.duplicate();
 
-    for (var layer_idx = 0; layer_idx <= selectedLayers.length - 1; layer_idx ++) {
-      // Get the first selected layer
-      var selectedLayer = selectedLayers[layer_idx];
-      var markerProperty = audio_markers
-
-      if (markerProperty) {
-        var numMarkers = markerProperty.numKeys; // Get the number of markers
-
-        for (var i = numMarkers; i >= 2; i--) {
-          var markerTime = markerProperty.keyTime(i); // Get the marker time
-
-          var next_marker_time = markerProperty.keyTime(i - 1);
-
-          // Duplicate the selected layer
-          var duplicatedLayer = selectedLayer.duplicate();
-
-          // Set the outPoint of the original layer to the marker time
-          selectedLayer.outPoint = markerTime;
-
-          // Set the inPoint of the duplicated layer to the marker time
-          duplicatedLayer.inPoint = next_marker_time;
-
-          duplicatedLayer.outPoint = markerTime;
-          duplicatedLayer.inPoint = next_marker_time;
-        }
-    } else {
-      alert("No markers found on the layer.");
-    }
-    }
-
-
-
-
-  } else {
-    alert("No layer selected in the timeline.");
-  }
-} else {
-  alert("No composition open in Adobe After Effects.");
+  
+  selectedLayer.outPoint = markerTime; // Set the outPoint of the original layer to the marker time
+  duplicatedLayer.inPoint = nextMarkerTime; // Set the inPoint of the duplicated layer to the marker time
+  
+  duplicatedLayer.outPoint = markerTime;
+  duplicatedLayer.inPoint = nextMarkerTime;
 }
